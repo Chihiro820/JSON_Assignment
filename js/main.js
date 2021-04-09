e9160f9c6fc42702f4ebbdf2254b9cb9b9b8f677
Chihiro Yoshimura <Chihiro.Yoshimura@cmeducations.se>
2021-04-09 20:08:32 +0200

    Assignment 9 APR

---
+++ js/main.js
@@ -0,0 +1,67 @@
+//Current time and date
+function showtime(){
+  var today = new Date();
+  $weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  $month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  let unko=$('#datetime').html($month[today.getMonth() + 1 ]+ " "+today.getDate() + " " +$weekday[today.getDay()] + " " +today.getHours() + ":" + ('0'+today.getMinutes()).slice(-2) + ":" + ('0' +today.getSeconds()).slice(-2));
+}
+setInterval(showtime,1000);
+//Finish Current time and date
+
+//Choose country
+const text = document.getElementById('text');
+text.addEventListener('input',covid);
+async function covid() {
+    try{
+//get value of country
+    let country= text.value;
+    console.log(country);
+//get value of data case
+    const dataCase = document.getElementById('dataCase').value;
+//get value of data type
+     const dataType = document.getElementById('dataType').value;
+//Some 2 values(dataCase & dataType)
+    let dataSum =dataType+dataCase;
+    console.log("dataSum is:"+dataSum);
+ //fetch data 
+    let response= await fetch('https://disease.sh/v3/covid-19/countries/'+country+'?yesterday=true&twoDaysAgo=true&allowNull=true');
+    console.log(response);
+    if(!response.ok) {
+            console.log('HTTP Error! status: ' + response.status);
+        }
+      let data = await response.json();
+      console.log(data);
+// //Define result value
+let result='';
+    if(dataSum == 'Totalrecovered'){
+        result=data.recovered;
+    }else if(dataSum == 'Todayrecovered'){
+        result=data.todayRecovered;
+    }else if(dataSum=='Totaldeaths'){
+        result=data.deaths;
+    }else if(dataSum=='Todaydeaths'){
+        result=data.todayDeaths;
+    }else{
+        result="Something error";
+    }
+    
+    console.log(result);
+
+
+      document.getElementById('output').innerHTML=`<div id="info">
+      <h1 class="country">${data.country}</h1>
+      <img class="country_img" src="${data.countryInfo.flag}">
+      </div>
+      <h3 id="datetime"></h3>
+      <h2>Confirmed cases today<h2><h1>${data.todayCases}</h1>
+      <h2>Total cases<h2><h1>${data.cases}</h1>
+      <h2>${dataType} ${dataCase}</h2>
+      <h1>${result}</h1>
+      `;console.log(result);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
